b806bde8d3037f6fd15fefc9cb891bef39187079
xGojo10 <m.syaifulrizal96@gmail.com>
2024-12-20 23:53:17 +0800

    landing page libraria

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.front', ['title' => 'Libraria | About'])
 @section('content')
-    <h1>About</h1>
+
+    <!-- about -->
+    <section class="h-custom">
+        <div class="container about-container my-5 bg-light rounded">
+            <div class="row w-100 my-5 px-5">
+                <div class="about my-5">
+                    <h2 class="mb-4">Tentang Kami</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nisi animi id neque itaque, a
+                        temporibus aut, fugit ut laboriosam laudantium ea corrupti atque non quae expedita, impedit
+                        accusantium. Nulla, quibusdam eum alias perspiciatis placeat numquam ipsam vitae necessitatibus
+                        atque! Totam assumenda veniam iusto provident, velit et perspiciatis. Similique, omnis.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/includes/frontsides/navbar.blade.php
+++ resources/views/includes/frontsides/navbar.blade.php
@@ -1,11 +1,16 @@
-<ul>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('home') }}">Home</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('front.about') }}">About Us</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('front.contact') }}">Contact Us</a>
-    </li>
-</ul>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="">Library</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="{{request()->routeIs('home') ? 'active' : ''}} nav-link" href="{{route('home')}}">Home</a></li>
+                    <li class="nav-item"><a class="{{request()->routeIs('front.about') ? 'active' : ''}} nav-link" href="{{route('front.about')}}">About</a></li>
+                    <li class="nav-item"><a class="{{request()->routeIs('front.contact') ? 'active' : ''}} nav-link" href="{{route('front.contact')}}">Contacts</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
